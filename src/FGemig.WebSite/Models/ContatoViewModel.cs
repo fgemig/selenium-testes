3150fcc2332985538f8fe3d1172c3feed944dec0
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-25 16:38:34 -0300

    Adicionando Data Annotations na viewmodel de Contato

--- src/FGemig.WebSite/Models/ContatoViewModel.cs
+++ src/FGemig.WebSite/Models/ContatoViewModel.cs
@@ -4,23 +4,23 @@ namespace FGemig.WebSite.Models
 {
     public class ContatoViewModel
     {
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [MaxLength(250, ErrorMessage = "Máximo de {0} caracteres permitidos")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = "O Campo {0}  obrigatório")]
         [MaxLength(100, ErrorMessage = "Máximo de {0} caracteres permitidos")]
         public string Assunto { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = "O Campo {0}  obrigatório")]
         [EmailAddress(ErrorMessage = "Email inválido")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = "O Campo {0}  obrigatório")]
         [MaxLength(15, ErrorMessage = "Máximo de {0} caracteres permitidos")]
         public string Telefone { get; set; }
 
-        [Required(ErrorMessage = "Campo obrigatório")]
+        [Required(ErrorMessage = "O Campo {0}  obrigatório")]
         [MaxLength(1000, ErrorMessage = "Máximo de {0} caracteres permitidos")]
         public string Mensagem { get; set; }
     }
