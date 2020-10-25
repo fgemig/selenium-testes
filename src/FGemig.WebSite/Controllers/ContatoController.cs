d9ae6aae318c4fc265bc878c4609e047e1f69e70
Fabio Gemignani <fabiogemignani@outlook.com>
2020-10-25 14:46:44 -0300

    Criação da página de Contato

---
+++ src/FGemig.WebSite/Controllers/ContatoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FGemig.WebSite.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly ILogger<ContatoController> _logger;
+
+        public ContatoController(ILogger<ContatoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
